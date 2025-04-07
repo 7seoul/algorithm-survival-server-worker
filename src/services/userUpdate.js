c127d7e90bdc88fd351c38ba7c5f67e1804a47fd
gonudayo <kgw010119@gmail.com>
2025-04-08 00:52:58 +0900

    fix: userUpdate

--- src/services/userUpdate.js
+++ src/services/userUpdate.js
@@ -1,5 +1,4 @@
 const scrap = require("../apis/scrap");
-const solvedac = require("../apis/solvedac");
 const { User } = require("../models/User/User");
 const { Group } = require("../models/Group/Group");
 const { MemberData } = require("../models/Group/MemberData");
@@ -27,6 +26,7 @@ const userUpdateByScrap = async (handle) => {
         initialStreak: newStreak,
         currentStreak: profile.streak,
         currentSolved: profile.solved,
+        score: profile.solved - initUser.initialSolved,
         imgSrc: profile.imgSrc,
         tier: profile.tier,
       };
@@ -123,130 +123,6 @@ const userUpdateByScrap = async (handle) => {
   }
 };
 
-const userUpdateBySolvedac = async (handle) => {
-  const label = `[USE SOLVEDAC API] "${handle}"`;
-  timer.start(label);
-  const profile = await solvedac.profile(handle);
-  const streak = await solvedac.grass(handle);
-  timer.end(label, logger);
-
-  if (profile && streak !== undefined) {
-    try {
-      const initUser = await User.findOne({ handle: handle });
-
-      let down = 0;
-      let newStreak = initUser.initialStreak;
-      if (initUser.initialStreak > streak) {
-        down = 1;
-        newStreak = streak;
-      }
-
-      const updateFields = {
-        initialStreak: newStreak,
-        currentStreak: streak,
-        currentSolved: profile.solvedCount,
-        imgSrc: profile.profileImageUrl,
-        tier: profile.tier,
-      };
-
-      // maxStreak 갱신 조건
-      if (
-        !initUser.maxStreak ||
-        streak - initUser.initialStreak > initUser.maxStreak
-      ) {
-        updateFields.maxStreak = streak - initUser.initialStreak;
-      }
-
-      const saved = await User.findOneAndUpdate(
-        { handle: handle },
-        { $set: updateFields },
-        { new: true }
-      )
-        .select("-__v -password -token -verificationCode")
-        .populate("joinedGroupList", "groupName _id memberData");
-
-      logger.info(`[USE SOLVEDAC API] "${handle}" profile updated`);
-
-      const groups = saved.joinedGroupList;
-      // 그룹에 유저 업데이트 정보 반영
-      for (let group of groups) {
-        const member = await MemberData.findOne(
-          { handle: handle, _id: { $in: group.memberData } },
-          { currentSolved: 1 }
-        );
-
-        if (!member) continue;
-
-        const previousSolved = member.currentSolved;
-        const solvedIncrease = profile.solvedCount - previousSolved;
-
-        // 유저 정보 업데이트
-        await MemberData.findOneAndUpdate(
-          { handle: handle, _id: { $in: group.memberData } },
-          {
-            $set: {
-              initialStreak: newStreak,
-              currentStreak: streak,
-              currentSolved: profile.solvedCount,
-            },
-            $inc: { downs: down },
-          }
-        );
-
-        // 그룹 점수 업데이트
-        if (solvedIncrease > 0) {
-          const updatedGroup = await Group.findByIdAndUpdate(
-            group._id,
-            {
-              $inc: { score: solvedIncrease },
-              $addToSet: { todaySolvedMembers: initUser._id },
-            },
-            { new: true }
-          ).select(
-            "todayAllSolved todaySolvedMembers members currentStreak maxStreak size"
-          );
-
-          const totalMembers = updatedGroup.size;
-          const solvedCount = updatedGroup.todaySolvedMembers.length;
-
-          if (solvedCount === totalMembers && !updatedGroup.todayAllSolved) {
-            // streak 증가
-            const newStreak = updatedGroup.currentStreak + 1;
-
-            const updateFields = {
-              $inc: { currentStreak: 1 },
-              $set: { todayAllSolved: true },
-            };
-
-            if (newStreak > updatedGroup.maxStreak) {
-              updateFields.$set.maxStreak = newStreak;
-            }
-
-            await Group.findByIdAndUpdate(group._id, updateFields);
-
-            logger.info(
-              `[USE SOLVEDAC API] 그룹 "${group.groupName}" streak 증가`
-            );
-          }
-        }
-
-        logger.info(
-          `[USE SOLVEDAC API] "${handle}" -> 그룹: "${group.groupName}" 점수 증가: ${solvedIncrease}`
-        );
-      }
-      return saved;
-    } catch (error) {
-      logger.error(
-        `[USE SOLVEDAC API] "${handle}" Error updating user:`,
-        error
-      );
-    }
-  } else {
-    logger.warn(`[USE SOLVEDAC API] "${handle}" FAIL TO SCRAPING.`);
-  }
-};
-
 module.exports = {
   userUpdateByScrap,
-  userUpdateBySolvedac,
 };
