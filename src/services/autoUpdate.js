cf604762f4be2042c3066ce99260b15343a83276
gonudayo <kgw010119@gmail.com>
2025-04-09 12:18:08 +0900

    hotfix: 오타수정

--- src/services/autoUpdate.js
+++ src/services/autoUpdate.js
@@ -71,7 +71,7 @@ const init = async () => {
   userQueue = await loadUsersFromDB();
 
   if (!userQueue || userQueue.length === 0) {
-    logger.wran("[AUTO] EMPTY QUEUE after reload! Retrying in 5s...");
+    logger.warn("[AUTO] EMPTY QUEUE after reload! Retrying in 5s...");
     setTimeout(init, 5000); // 5초 후 재시도
     return;
   }
